d5d39d522fd85679f95b7126b0cf7974d86d98ec
Tyago <santiagox46@hotmail.com>
2025-07-11 10:42:44 -0400

    Initial Commit

---
+++ DictionaryApp-API/DictionaryApp_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp_APIApp.swift
+//  DictionaryApp-API
+//
+//  Created by Tyago on 2025-07-11.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
